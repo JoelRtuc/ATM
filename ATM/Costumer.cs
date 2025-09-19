983a847adeacf5eff47c4ebabe9f8faccc4f155a
Joel Ringh <joel.ringh@gmail.com>
2025-09-19 18:07:41 +0200

    Accidentally pulled before pushing. Pushing new commit

---
+++ ATM/Costumer.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATM
+{
+    internal class Costumer
+    {
+        int pin;
+        string name;//ALWAYS USE LOWERCASE 
+        double balance;
+
+        public Costumer()
+        {
+            this.pin = 0;
+            this.name = "";
+            this.balance = 0;
+        }
+
+        public Costumer(int pin, string name, double balance)
+        {
+            this.pin = pin;
+            this.name = name;
+            this.balance = balance;
+        }
+
+        public Costumer ActivateAccount(int pin, string name)
+        {
+            if(pin == this.pin && name == this.name)
+            {
+                return this;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public void Deposit(double amount)
+        {
+            balance += amount;
+        }
+
+        public void Withdraw(double amount)
+        {
+            if (amount > balance || amount < 0)
+            {
+                Console.WriteLine("Insufficient funds");
+            }
+            else
+            {
+                balance -= amount;
+            }
+        }
+
+        public void CheckBalance()
+        {
+            Console.WriteLine($"Your balance is: {balance}");
+        }
+    }
+}
--- ATM/Program.cs
+++ ATM/Program.cs
@@ -4,7 +4,69 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int pin;
+            string name;
+            List<Costumer> costumers = new List<Costumer>();
+            Costumer selectedCostumer = new Costumer();
+
+            costumers.Add(new Costumer(1234, "john doe", 1000.0));
+            costumers.Add(new Costumer(1235, "jane doe", 2000.0));
+
+            while (true)
+            {
+                Console.WriteLine("Enter Pin");
+                pin = int.TryParse(Console.ReadLine(), out pin) ? pin : 0;
+                Console.WriteLine("Enter Name");
+                name = Console.ReadLine();
+
+                foreach (Costumer costumer in costumers)
+                {
+                    selectedCostumer = costumer.ActivateAccount(pin, name.ToLower());
+
+                    if (selectedCostumer != null)
+                    {
+                        break;
+                    }
+                }
+
+                if (selectedCostumer == null)
+                {
+                    Console.WriteLine("Invalid credentials");
+                    continue;
+                }
+
+                Console.WriteLine("Select an option:\n1. Deposit\n2. Withdraw\n3. Check Balance\n4. Exit");
+
+                int selectedOption = int.TryParse(Console.ReadLine(), out selectedOption) ? selectedOption : 0;
+
+                switch (selectedOption)
+                {
+                    case 1:
+                        Console.WriteLine("Enter amount to deposit:");
+                        double depositAmount = double.TryParse(Console.ReadLine(), out depositAmount) ? depositAmount : 0;
+                        selectedCostumer.Deposit(depositAmount);
+                        Console.WriteLine("Deposit successful");
+                        break;
+                    case 2:
+                        Console.WriteLine("Enter amount to withdraw:");
+                        double withdrawAmount = double.TryParse(Console.ReadLine(), out withdrawAmount) ? withdrawAmount : 0;
+                        selectedCostumer.Withdraw(withdrawAmount);
+                        Console.WriteLine("Withdrawal successful");
+                        break;
+                    case 3:
+                        selectedCostumer.CheckBalance();
+                        break;
+                    case 4:
+                        Console.WriteLine("Have a nice day");
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Invalid option");
+                        continue;
+
+                }
+            }
+
         }
     }
 }
